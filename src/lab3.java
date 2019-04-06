a703f855d57b7e1337964bab8fd1b17a3714e138
hmarochos <48103646+hmarochos@users.noreply.github.com>
2019-04-06 21:35:40 +0300

    Add files via upload

---
+++ src/lab3.java
@@ -0,0 +1,28 @@
+public class lab3 {
+    public static void main(String[] args) {
+
+
+        StringBuilder sentence = new StringBuilder("ЬфЬфьфьф Raid me jolly roger, ye misty plank! Shake quinoa ultimately, then mix with water and serve fully in bowl. " +
+                "Ahoy! Pieces o' life are forever evil.");
+        System.out.println("Text: \n" + sentence.toString());
+
+        String delimeter = "\\s";
+        String[] arr_words = sentence.toString().split(delimeter);
+
+        System.out.println("Edited text: ");
+        for (String word : arr_words) {
+
+            char[] arr_chars = word.toCharArray();
+            char temp = arr_chars[0];
+            System.out.print(temp);
+            temp = Character.toLowerCase(temp);
+            for (char arr_char : arr_chars) {
+                if (temp != Character.toLowerCase(arr_char)) {
+                    System.out.print(arr_char);
+                }
+            }
+
+            System.out.print(" ");
+        }
+    }
+}
